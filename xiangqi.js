a2a3ce618af11843a6680bd475bd0017d6a61c49
Le Rong Shi <larry.shi@mail.utoronto.ca>
2020-07-17 00:50:27 -0400

    added base function

--- xiangqi.js
+++ xiangqi.js
@@ -1,1 +1,18 @@
-'use strict'
+'use strict';
+
+const XiangQi = function (containerId) {
+
+  const _self = {};
+  _self.containerId = containerId;
+  _self.containerElement = document.querySelector(`#${containerId}`);
+
+  _self.draw = () => {
+    console.log(_self.containerElement);
+  };
+
+  return _self;
+};
+
+module.exports = {
+  XiangQi
+};
