249791a886bd3206b48b12453da1c3ce7f33e5f4
Le Rong Shi <larry.shi@mail.utoronto.ca>
2020-07-18 13:04:34 -0400

    added simple grid

--- xiangqi.js
+++ xiangqi.js
@@ -2,17 +2,50 @@
 
 const XiangQi = function (containerId) {
 
+  const CSS = {
+    board: 'board',
+    clearFix: 'clearFix',
+    row: 'row',
+    square: 'square',
+  };
+
   const _self = {};
   _self.containerId = containerId;
   _self.containerElement = document.querySelector(`#${containerId}`);
+  _self.board = [];
 
   _self.draw = () => {
-    console.log(_self.containerElement);
+    const board = createDiv(CSS.board);
+    const squareSize = 48;
+
+    for (let i = 0; i < 9; i++) {
+      if (i === 4) {
+
+      }
+      const row = createDiv(CSS.row);
+
+      for (let j = 0; j < 8; j++) {
+        const square = createDiv(CSS.square);
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        row.appendChild(square);
+      }
+
+      const clearFix = createDiv(CSS.clearFix);
+      row.appendChild(clearFix);
+
+      board.appendChild(row);
+    }
+
+    _self.containerElement.appendChild(board);
   };
 
   return _self;
 };
 
-module.exports = {
-  XiangQi
-};
+
+const createDiv = (className) => {
+  const element = document.createElement('div');
+  element.className = className
+  return element
+}
