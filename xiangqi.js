df5027add75589da09d42d6fc8c2696c888f969a
Le Rong Shi <larry.shi@mail.utoronto.ca>
2020-07-18 14:38:11 -0400

    fixed board

--- xiangqi.js
+++ xiangqi.js
@@ -1,13 +1,20 @@
 'use strict';
 
-const XiangQi = function (containerId) {
+const CSS = {
+  board: 'board',
+  clearFix: 'clearFix',
+  row: 'row',
+  square: 'square',
+  squareTopRow: 'square-top-row',
+  squareBotRow: 'square-bottom-row',
+  squareLeftCol: 'square-left-col',
+  squareRightCol: 'square-right-col',
+  squareNoBorder: 'square-no-border'
+};
 
-  const CSS = {
-    board: 'board',
-    clearFix: 'clearFix',
-    row: 'row',
-    square: 'square',
-  };
+
+// Main library function
+const XiangQi = function (containerId) {
 
   const _self = {};
   _self.containerId = containerId;
@@ -15,25 +22,21 @@ const XiangQi = function (containerId) {
   _self.board = [];
 
   _self.draw = () => {
-    const board = createDiv(CSS.board);
+    const board = createDiv([CSS.board]);
     const squareSize = 48;
 
     for (let i = 0; i < 9; i++) {
-      if (i === 4) {
-
-      }
-      const row = createDiv(CSS.row);
+      const row = createDiv([CSS.row]);
 
       for (let j = 0; j < 8; j++) {
-        const square = createDiv(CSS.square);
+        const square = createDiv(getSquareClass(i, j));
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
         row.appendChild(square);
       }
 
-      const clearFix = createDiv(CSS.clearFix);
+      const clearFix = createDiv([CSS.clearFix]);
       row.appendChild(clearFix);
-
       board.appendChild(row);
     }
 
@@ -46,6 +49,32 @@ const XiangQi = function (containerId) {
 
 const createDiv = (className) => {
   const element = document.createElement('div');
-  element.className = className
-  return element
-}
+  element.classList.add(...className);
+  return element;
+};
+
+const getSquareClass = (row, col) => {
+  const classes = [];
+  switch (row) {
+    case 0:
+      classes.push(CSS.squareTopRow);
+      break;
+    case 4:
+      classes.push(CSS.squareNoBorder);
+      break;
+    case 8:
+      classes.push(CSS.squareBotRow);
+      break;
+    default:
+      classes.push(CSS.square);
+  }
+
+  switch (col) {
+    case 0:
+      classes.push(CSS.squareLeftCol);
+      break;
+    case 7:
+      classes.push(CSS.squareRightCol);
+  }
+  return classes
+};
