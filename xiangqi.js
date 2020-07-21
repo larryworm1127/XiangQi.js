e89de654e793826e4e6ba1813c0233ddc7e0282a
Le Rong Shi <larry.shi@mail.utoronto.ca>
2020-07-20 21:52:41 -0400

    switched to using svg for board background

--- xiangqi.js
+++ xiangqi.js
@@ -3,61 +3,13 @@
 const CSS = {
   board: 'board',
   boardContainer: 'board-container',
-  clearFix: 'clearFix',
-  row: 'row',
-  square: 'square',
-  crossLeft: 'cross-left',
-  crossRight: 'cross-right',
-  squareTopRow: 'square-top-row',
-  squareBotRow: 'square-bottom-row',
-  squareLeftCol: 'square-left-col',
-  squareRightCol: 'square-right-col',
-  squareNoBorder: 'square-no-border',
 };
 
-const CROSS_LEFT_RATIO = -1.4375;
-const CROSS_TOP_RATIO = 0.583;
 
-
-class Draw {
-
-  createSquareDom = function (row, col) {
-    const square = document.createElement('div');
-    square.style.width = `${this.squareSize}px`;
-    square.style.height = `${this.squareSize}px`;
-    square.classList.add(...getSquareClass(row, col));
-
-    // Draw right cross
-    if (col === 3 && (row === 0 || row === 7)) {
-      const cross = document.createElement('div');
-      cross.style.width = `${this.squareCrossLength}px`;
-      cross.style.height = `${this.squareCrossLength}px`;
-      cross.style.top = `${this.squareSize * CROSS_TOP_RATIO}px`;
-      cross.style.left = `${this.squareSize * CROSS_LEFT_RATIO}px`;
-      cross.className = CSS.crossRight;
-      square.appendChild(cross);
-    }
-
-    // Draw left cross
-    if (col === 5 && (row === 0 || row === 7)) {
-      const cross = document.createElement('div');
-      cross.style.width = `${this.squareCrossLength}px`;
-      cross.style.height = `${this.squareCrossLength}px`;
-      cross.style.left = `${this.squareSize * CROSS_LEFT_RATIO}px`;
-      cross.style.top = `${this.squareSize * CROSS_TOP_RATIO}px`;
-      cross.className = CSS.crossLeft;
-      square.appendChild(cross);
-    }
-    return square;
-  };
-}
-
-
-class XiangQi extends Draw {
+class XiangQi {
 
   constructor(containerId, boardSize) {
-    super();
-    this.boardWidth = (boardSize === undefined) ? 402 : boardSize;
+    this.boardWidth = (boardSize === undefined) ? 400 : boardSize;
     this.squareSize = (this.boardWidth - 2) / 8 - 2;
     this.boardHeight = (this.squareSize + 2) * 9 + 2;
     this.squareCrossLength = Math.sqrt(Math.pow(this.squareSize, 2) * 2) * 2 + 3;
@@ -70,19 +22,19 @@ class XiangQi extends Draw {
     boardContainer.style.width = `${this.boardWidth}px`;
     boardContainer.style.height = `${this.boardHeight}px`;
     const board = createDiv([CSS.board]);
-    board.style.width = this.boardWidth;
-    board.style.height = this.boardHeight;
+    board.style.width = `${this.boardWidth}px`;
+    board.style.height = `${this.boardHeight}px`;
 
     for (let i = 0; i < 9; i++) {
-      const row = createDiv([CSS.row]);
+      const row = createDiv([]);
 
       for (let j = 0; j < 8; j++) {
-        const square = this.createSquareDom(i, j);
+        const square = createDiv([]);
         row.appendChild(square);
         this.board[i].push(square);
       }
 
-      const clearFix = createDiv([CSS.clearFix]);
+      const clearFix = createDiv([]);
       row.appendChild(clearFix);
       board.appendChild(row);
       boardContainer.appendChild(board);
@@ -90,44 +42,10 @@ class XiangQi extends Draw {
 
     this.containerElement.appendChild(boardContainer);
   };
-
-  drawPieces = function () {
-    this.board.forEach((item) => {
-      item.forEach((square) => {
-        square.appendChild(createDiv(['piece']));
-      });
-    });
-  };
 }
 
 const createDiv = (className) => {
   const element = document.createElement('div');
   element.classList.add(...className);
   return element;
-};
-
-const getSquareClass = (row, col) => {
-  const classes = [];
-  switch (row) {
-    case 0:
-      classes.push(CSS.squareTopRow);
-      break;
-    case 4:
-      classes.push(CSS.squareNoBorder);
-      break;
-    case 8:
-      classes.push(CSS.squareBotRow);
-      break;
-    default:
-      classes.push(CSS.square);
-  }
-
-  switch (col) {
-    case 0:
-      classes.push(CSS.squareLeftCol);
-      break;
-    case 7:
-      classes.push(CSS.squareRightCol);
-  }
-  return classes;
 };
