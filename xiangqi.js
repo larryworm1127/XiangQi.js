ceafcb05a8e016bcba53029820fbab7173529925
Le Rong Shi <larry.shi@mail.utoronto.ca>
2020-07-21 12:23:30 -0400

    added pieces

--- xiangqi.js
+++ xiangqi.js
@@ -1,48 +1,88 @@
 'use strict';
 
+// Other constants
 const CSS = {
   board: 'board',
   boardContainer: 'board-container',
+  square: 'square',
+  clear: 'clear',
+  row: 'row',
+};
+const PIECE_PATH = 'assets/pieces/';
+
+// XiangQi related constants
+const NUM_ROWS = 10;
+const NUM_COLS = 9;
+const PIECES = {
+  general: 'General.svg',
+  advisor: 'Advisor.svg',
+  elephant: 'Elephant.svg',
+  cannon: 'Cannon.svg',
+  chariot: 'Chariot.svg',
+  horse: 'Horse.svg',
+  soldier: 'Soldier.svg'
+};
+const SIDES = {
+  red: 'r',
+  black: 'b'
 };
 
 
-class XiangQi {
+function XiangQi(containerId, boardSize) {
+  this.boardWidth = (boardSize === undefined) ? 400 : boardSize;
+  this.squareSize = this.getSquareSize();
+  this.boardHeight = (this.boardWidth / 9) * 10;
+  this.containerElement = document.querySelector(`#${containerId}`);
+  this.board = [[], [], [], [], [], [], [], [], [], []];
+}
 
-  constructor(containerId, boardSize) {
-    this.boardWidth = (boardSize === undefined) ? 400 : boardSize;
-    this.squareSize = (this.boardWidth - 2) / 8 - 2;
-    this.boardHeight = (this.squareSize + 2) * 9 + 2;
-    this.squareCrossLength = Math.sqrt(Math.pow(this.squareSize, 2) * 2) * 2 + 3;
-    this.containerElement = document.querySelector(`#${containerId}`);
-    this.board = [[], [], [], [], [], [], [], [], []];
-  }
 
-  draw = function () {
+XiangQi.prototype = {
+
+  draw: function () {
     const boardContainer = createDiv([CSS.boardContainer]);
-    boardContainer.style.width = `${this.boardWidth}px`;
-    boardContainer.style.height = `${this.boardHeight}px`;
     const board = createDiv([CSS.board]);
     board.style.width = `${this.boardWidth}px`;
     board.style.height = `${this.boardHeight}px`;
 
-    for (let i = 0; i < 9; i++) {
-      const row = createDiv([]);
+    for (let row = 0; row < NUM_ROWS; row++) {
+      const rowDiv = createDiv([CSS.row]);
 
-      for (let j = 0; j < 8; j++) {
-        const square = createDiv([]);
-        row.appendChild(square);
-        this.board[i].push(square);
+      for (let col = 0; col < NUM_COLS; col++) {
+        const square = createDiv([CSS.square]);
+        square.style.width = `${this.squareSize}px`;
+        square.style.height = `${this.squareSize}px`;
+        rowDiv.appendChild(square);
+        this.board[row].push(square);
       }
 
-      const clearFix = createDiv([]);
-      row.appendChild(clearFix);
-      board.appendChild(row);
+      const clear = createDiv([CSS.clear]);
+      rowDiv.appendChild(clear);
+      board.appendChild(rowDiv);
       boardContainer.appendChild(board);
     }
 
     this.containerElement.appendChild(boardContainer);
-  };
-}
+  },
+
+  drawPieces: function (row, col, piece, side) {
+    const pieceElement = document.createElement('img');
+    pieceElement.src = `${PIECE_PATH}${SIDES[side]}${PIECES[piece]}`;
+    pieceElement.style.width = `${this.squareSize}px`;
+    pieceElement.style.height = `${this.squareSize}px`;
+    this.board[row][col].appendChild(pieceElement);
+  },
+
+  getSquareSize: function () {
+    let boardWidthContainer = this.boardWidth - 1;
+
+    while (boardWidthContainer % NUM_COLS !== 0 && boardWidthContainer > 0) {
+      boardWidthContainer = boardWidthContainer - 1;
+    }
+
+    return boardWidthContainer / NUM_COLS;
+  }
+};
 
 const createDiv = (className) => {
   const element = document.createElement('div');
