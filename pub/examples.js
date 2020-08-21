7c9ed2fcdd150ba297117995d7c5419b88f05144
Le Rong Shi <larry.shi@mail.utoronto.ca>
2020-08-21 01:39:49 -0400

    fixed more stuff

--- pub/examples.js
+++ pub/examples.js
@@ -3,69 +3,16 @@
 
 // Board demo functions
 const emptyBoard = function () {
-  const fullConfig = {
-    // the size of the XiangQi board, defaults to 400 if not provided
-    boardSize: 400,
-    // the ID of container element in the DOM, defaults to document.body if not provided
-    containerId: 'emptyBoard',
-    // initial board content, defaults to empty if not provided
-    boardContent: [],
-    // whether to draw board with start positions, defaults to false if not provided.
-    startPos: false,
-    // whether to draw sidebar or not, defaults to false if not provided (sidebar not implemented yet)
-    showSideBar: false,
-    // whether to allow pieces to be draggable, defaults to false if not provided (dragging not implemented yet)
-    draggable: false,
-    // whether to draw board on object creation, defaults to false if not provided.
-    delayDraw: false,
-    // whether to draw red side on bottom for start positions, defaults to false if not provided.
-    redOnBottom: false
-  };
-  const board = new XiangQi(fullConfig);
+  const board = new XiangQi({ containerId: 'emptyBoard' });
 };
 
 const fullBoardStart = function () {
-  const board = new XiangQi({ containerId: 'fullBoardStart', startPos: true });
+  const board = new XiangQi({ containerId: 'fullBoardStart', boardContent: 'start' });
 };
 
 const midGameBoard = function () {
-  const midGameBoardContent = [
-    [
-      Piece(PIECES.empty), Piece(PIECES.chariot, SIDES.black),
-      Piece(PIECES.empty), Piece(PIECES.empty), Piece(PIECES.general, SIDES.black),
-      Piece(PIECES.empty), Piece(PIECES.empty), Piece(PIECES.empty), Piece(PIECES.empty),
-    ],
-    new Array(9).fill(Piece(PIECES.empty)),
-    [
-      Piece(PIECES.empty), Piece(PIECES.horse, SIDES.black), Piece(PIECES.empty),
-      Piece(PIECES.empty), Piece(PIECES.elephant, SIDES.black), Piece(PIECES.advisor, SIDES.black),
-      Piece(PIECES.empty), Piece(PIECES.empty), Piece(PIECES.empty),
-    ],
-    new Array(9).fill(Piece(PIECES.empty)).map((item, index) => {
-      return (index === 4) ? Piece(PIECES.soldier, SIDES.black) : item;
-    }),
-    [
-      Piece(PIECES.empty), Piece(PIECES.chariot, SIDES.red), Piece(PIECES.empty),
-      Piece(PIECES.empty), Piece(PIECES.empty), Piece(PIECES.soldier, SIDES.red),
-      Piece(PIECES.empty), Piece(PIECES.empty), Piece(PIECES.empty),
-    ],
-    new Array(9).fill(Piece(PIECES.empty)).map((item, index) => {
-      return (index === 7) ? Piece(PIECES.horse, SIDES.red) : item;
-    }),
-    [
-      Piece(PIECES.soldier, SIDES.black), Piece(PIECES.empty), Piece(PIECES.empty),
-      Piece(PIECES.empty), Piece(PIECES.cannon, SIDES.red), Piece(PIECES.empty),
-      Piece(PIECES.empty), Piece(PIECES.cannon, SIDES.black), Piece(PIECES.chariot, SIDES.black)
-    ],
-    new Array(9).fill(Piece(PIECES.empty)).map((item, index) => {
-      return (index === 3) ? Piece(PIECES.advisor, SIDES.red) : item;
-    }),
-    new Array(9).fill(Piece(PIECES.empty)).map((item, index) => {
-      return (index === 4) ? Piece(PIECES.general, SIDES.red) : item;
-    }),
-    new Array(9).fill(Piece(PIECES.empty))
-  ];
-  const board = new XiangQi({ containerId: 'midGameBoard', boardContent: midGameBoardContent });
+  const midGameFen = '1r2g4/9/1h2ea3/4s4/1R3S3/7H1/s3C2cr/3A5/4G4/9'
+  const board = new XiangQi({ containerId: 'midGameBoard', boardContent: midGameFen });
 };
 
 const interactiveBoard = function () {
